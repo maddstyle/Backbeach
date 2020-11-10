d48fd51485f8df0933fb6bf16e91933f67e87933
maddstyle <michaela.decamara@gmail.com>
2020-11-09 23:37:19 -0500

    call to action section done

--- backbeach-wp/wp-content/themes/understrap-master/page-templates/template-home.php
+++ backbeach-wp/wp-content/themes/understrap-master/page-templates/template-home.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Template: Home
  *
@@ -8,15 +9,52 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 ?>
-<div class="home">
-<div class="home-banner">
 
+
+<div class="home-banner">
+  <div class="container">
+    <h1>A Dining Experience</h1>
+  </div>
 </div>
+
+<div class="home-call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <a href="#" class="call-to-action">
+          <div class="image"></div>
+          <div class="title">Dining Menu</div>
+        </a>
+      </div>
+      <div class="col-md-4">
+        <a href="#" class="call-to-action second">
+          <div class="image"></div>
+          <div class="title">Lunch Menu</div>
+        </a>
+      </div>
+      <div class="col-md-4">
+        <a href="#" class="call-to-action third">
+          <div class="image"></div>
+          <div class="title">Events</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="home-welcome">
+  <div class="container">
+    <h2>
+      <span>Welcome to</span>
+      The Backbeach Eating House
+    </h2>
+  </div>
 </div>
 
 <?php
 get_footer();
+/* call-to-action-1 */
