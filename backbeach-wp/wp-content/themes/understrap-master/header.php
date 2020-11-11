29cdaaee5ac1055f3fed00219a63098825a123cd
maddstyle <michaela.decamara@gmail.com>
2020-11-10 23:37:50 -0500

    valentines day section done

--- backbeach-wp/wp-content/themes/understrap-master/header.php
+++ backbeach-wp/wp-content/themes/understrap-master/header.php
@@ -20,7 +20,8 @@ $container = get_theme_mod('understrap_container_type');
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
--- backbeach-wp/wp-content/themes/understrap-master/page-templates/template-home.php
+++ backbeach-wp/wp-content/themes/understrap-master/page-templates/template-home.php
@@ -47,11 +47,60 @@ get_header();
 </div>
 
 <div class="home-welcome">
+  <div class="title-holder">
+    <div class="container">
+      <h2>
+        <span>Welcome to</span><br>
+        The Backbeach Eating House
+      </h2>
+    </div>
+  </div>
   <div class="container">
-    <h2>
-      <span>Welcome to</span>
-      The Backbeach Eating House
-    </h2>
+    <div class="row">
+      <div class="col-lg-1"></div>
+      <div class="col-lg-8">
+        <p class="tagline">
+          There is only one place like the BackBeach Cafe & Restaurant. Located right on the beach in beautiful Delray area. Offers alfresco seating and an elevated, open-air dining room highlighted by granite, authentic artwork and magnificent woodwork.
+        </p>
+        <p>
+          Headed by a team of Italian chefs, the kitchen uses only the finest ingredients available in the preparation of dishes that are exceptionally flavorful.
+          We are proud to support local business and producers by sourcing as much as possible locally, including fish from Key West, beef and chicken from Loxahatchee and vegetables from our own Bedner's Farms in the heart of Delray Beach.
+        </p>
+      </div>
+      <div class="col-lg-3">
+        <div class="button">
+          About Us<i class="fa fa-chevron-right"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="home-news">
+  <div class="bg-image"></div>
+  <div class="container">
+    <div class="row aligh-items-center">
+      <div class="col-xl-5 col-lg-6">
+        <a href="#" class="news-img">
+          <div class="image"></div>
+          <div class="date">Fri Feb 14th</div>
+          <div class="category">What's On</div>
+        </a>
+      </div>
+      <!-- use this one column as a space -->
+      <div class="col-xl-1 d-none d-xl-block">
+      </div>
+      <div class="col-lg-6">
+        <h2>Valentine's Day</h2>
+        <div class="content-holder">
+          <p>Join us and Celebrate Love with a special tasting menu featuring Steak and Seafood. Lunch is pretty full but we're taking dinner bookings for our full a la carte dinner menu for two. Have a romantic dinner on the beach with your loved one. All dinner reservations receive a complimentary box of chocolates too...
+          </p>
+        </div>
+        <div href="#" class="button">
+          Read More<i class="fa fa-chevron-right"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
